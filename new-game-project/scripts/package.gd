587a03439644d2178c8765ff7869b03271f573c4
hugobiella <hugo.a.biella@hotmail.com>
2024-09-20 20:23:58 -0300

    package pickup interaction!

--- new-game-project/scripts/package.gd
+++ new-game-project/scripts/package.gd
@@ -1,4 +1,4 @@
-extends RigidBody2D
+extends StaticBody2D
 
 @onready var interaction_area: InteractionArea = $InteractionArea
 
@@ -6,4 +6,8 @@ func _ready():
 	interaction_area.interact = Callable(self, "_on_interact")
 
 func _on_interact():
-	print("interaction!")
+	var player = get_tree().get_first_node_in_group("player") as CharacterBody2D
+	if player.carrying_object == null:
+		player.pickup_object(self)
+	else:
+		player.drop_object()
--- new-game-project/scripts/player.gd
+++ new-game-project/scripts/player.gd
@@ -1,5 +1,8 @@
 extends CharacterBody2D
 
+var pickup_offset  # package position
+var carrying_object = null
+
 const ACCELERATION = 10000
 const MAX_SPEED = 500
 
@@ -7,19 +10,25 @@ const MAX_SPEED = 500
 
 func _physics_process(delta):
 	move(delta)
+	
+	if carrying_object:
+		carrying_object.position = global_position + pickup_offset
 
 func move(delta):
 	var input_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	if input_vector == Vector2.ZERO:
 		velocity = Vector2.ZERO
 		sprite.play("idle")
+		pickup_offset = Vector2(0, -60)
 	else:
 		apply_movement(input_vector * ACCELERATION * delta)
 		move_and_slide()
 		if abs(input_vector.x) > abs(input_vector.y):
 			if input_vector.x > 0:
 				sprite.play("run_right")
+				pickup_offset = Vector2(60, 0)
 			else:
+				pickup_offset = Vector2(-60, 0)
 				sprite.play("run_left")
 		else:
 			if input_vector.y > 0:
@@ -28,6 +37,18 @@ func move(delta):
 				sprite.play("run_up")
 
 func apply_movement(amount) -> void:
-	#print(velocity)
 	velocity += amount
 	velocity = velocity.limit_length(MAX_SPEED)
+
+func pickup_object(object: StaticBody2D):
+	carrying_object = object
+	object.get_parent().remove_child(object)
+	get_parent().add_child(object)
+	carrying_object.position = global_position + pickup_offset
+
+func drop_object():
+	if carrying_object:
+		carrying_object.get_parent().remove_child(carrying_object)
+		get_parent().add_child(carrying_object)
+		carrying_object.position = global_position + pickup_offset
+		carrying_object = null
