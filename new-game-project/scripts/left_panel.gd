671bd57aff7075b3682828a704b5735ed4dcb5fc
hugobiella <hugo.a.biella@hotmail.com>
2024-11-19 19:24:53 -0300

    1.0!!!

--- new-game-project/scripts/left_panel.gd
+++ new-game-project/scripts/left_panel.gd
@@ -1,7 +1,78 @@
-extends Panel
+extends Control
+
+var virtual_addresses = Globals.virtual_address_array
+var physical_addresses = Globals.physical_address_array
+var shuffled_indices = []
+var current_index = 0
+
+@onready var label_va = $Label_va
+@onready var label_check = $Label_check
+@onready var le_pa = $LE_pa
+@onready var le_offset = $LE_offset
+@onready var button = $Button
 
 func _ready():
-	pass
-	
-func _process(_delta):
-	pass
+	initialize_random_order()
+	update_virtual_address()
+	label_check.text = ""
+	button.disabled = false
+
+func initialize_random_order():
+	shuffled_indices = []
+	for i in range(virtual_addresses.size()):
+		shuffled_indices.append(i)
+	shuffled_indices.shuffle()
+
+func update_virtual_address():
+	if current_index < shuffled_indices.size():
+		var index = shuffled_indices[current_index]
+		var virtual_address_without_offset = String("%X" % virtual_addresses[index]).substr(0, String("%X" % virtual_addresses[index]).length() - 3)
+		label_va.text = virtual_address_without_offset
+
+		var virtual_address = String("%X" % virtual_addresses[index]).to_upper()
+		var expected_offset = virtual_address.substr(virtual_address.length() - 3, 3)
+
+		var expected_pa = ""
+		for i in range(physical_addresses.size()):
+			var physical_address = String("%X" % physical_addresses[i]).to_upper()
+			var physical_offset = physical_address.substr(physical_address.length() - 3, 3)
+
+			if physical_offset == expected_offset:
+				expected_pa = physical_address.substr(0, 5)
+				break
+
+		le_pa.placeholder_text = "Endereço Físico"
+		le_offset.placeholder_text = "Deslocamento"
+	else:
+		label_check.text = "Jogo concluído!"
+		label_va.text = ""
+		button.disabled = true
+
+func _on_button_pressed():
+	if current_index >= shuffled_indices.size():
+		return
+
+	var index = shuffled_indices[current_index]
+	var input_pa = le_pa.text.strip_edges().to_upper()
+	var input_offset = le_offset.text.strip_edges().to_upper()
+
+	var virtual_address = String("%X" % virtual_addresses[index]).to_upper()
+	var expected_offset = virtual_address.substr(virtual_address.length() - 3, 3)
+
+	var expected_pa = ""
+	for i in range(physical_addresses.size()):
+		var physical_address = String("%X" % physical_addresses[i]).to_upper()
+		var physical_offset = physical_address.substr(physical_address.length() - 3, 3)
+
+		if physical_offset == expected_offset:
+			expected_pa = physical_address.substr(0, 5)
+			break
+
+	if input_pa == expected_pa and input_offset == expected_offset:
+		label_check.text = "Tradução correta!"
+		current_index += 1
+		update_virtual_address()
+		le_pa.text = ""
+		le_offset.text = ""
+	else:
+		label_check.text = "Tradução incorreta!\nTente novamente."
--- new-game-project/scripts/phase_2.gd
+++ new-game-project/scripts/phase_2.gd
@@ -1,8 +1,9 @@
 extends Control
 @onready var grid_container: GridContainer = $RightPanel/GridContainer
+@onready var le_pa: LineEdit = $LeftPanel/LE_pa
+@onready var le_offset: LineEdit = $LeftPanel/LE_offset
 
 func _ready():
-	
 	print("Virtual Address Array:")
 	for address in Globals.virtual_address_array:
 		print("%08X" % address)
@@ -21,7 +22,7 @@ func _ready():
 	grid_container.add_child(create_label(" "))
 	grid_container.add_child(create_label(" "))
 	grid_container.add_child(create_label(" "))
-	
+
 	for virtual_address in Globals.virtual_address_array:
 		var offset_virtual = extract_offset(virtual_address)
 		var formatted_virtual_address = virtual_address >> 12
@@ -34,9 +35,11 @@ func _ready():
 			if offset_virtual == offset_physical:
 				var formatted_physical_address = physical_address >> 12
 				formatted_physical_address = "%05X" % formatted_physical_address
+
 				grid_container.add_child(create_label(formatted_virtual_address))
-				grid_container.add_child(create_label(formatted_physical_address))
-				grid_container.add_child(create_label(formatted_offset_virtual))
+				grid_container.add_child(create_button(formatted_physical_address, le_pa))
+				grid_container.add_child(create_button(formatted_offset_virtual, le_offset))
+				
 				print("Endereço Virtual e Físico correspondem: ", formatted_virtual_address, " --> ", formatted_physical_address)
 				break
 
@@ -45,6 +48,16 @@ func create_label(text) -> Label:
 	label.text = text
 	label.add_theme_font_size_override("font_size", 21)
 	return label
+
+func create_button(text, target_line_edit: LineEdit) -> Button:
+	var button = Button.new()
+	button.text = text
+	button.add_theme_font_size_override("font_size", 21)
+	button.pressed.connect(Callable(self, "_on_button_pressed").bind(text, target_line_edit))
+	return button
+
+func _on_button_pressed(value: String, target_line_edit: LineEdit):
+	target_line_edit.text = value
 
 func extract_offset(address):
 	var offset = address & 0xFFF
