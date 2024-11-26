1e824f4c3930785e856c5ec3faf64da25ae49984
hugobiella <hugo.a.biella@hotmail.com>
2024-11-26 17:49:30 -0300

    updates and fixes! 26/11
    fixing expected PAs not working with "0" on the start

--- new-game-project/scripts/left_panel.gd
+++ new-game-project/scripts/left_panel.gd
@@ -10,6 +10,8 @@ var current_index = 0
 @onready var le_pa = $LE_pa
 @onready var le_offset = $LE_offset
 @onready var button = $Button
+@onready var winsfx = $winsfx
+@onready var losesfx = $losesfx
 
 func _ready():
 	initialize_random_order()
@@ -54,23 +56,29 @@ func _on_button_pressed():
 	var input_pa = le_pa.text.strip_edges().to_upper()
 	var input_offset = le_offset.text.strip_edges().to_upper()
 
-	var virtual_address = String("%X" % virtual_addresses[index]).to_upper()
+	var virtual_address = String("%08X" % virtual_addresses[index]).to_upper()
 	var expected_offset = virtual_address.substr(virtual_address.length() - 3, 3)
 
 	var expected_pa = ""
 	for i in range(physical_addresses.size()):
-		var physical_address = String("%X" % physical_addresses[i]).to_upper()
+		var physical_address = String("%08X" % physical_addresses[i]).to_upper()
 		var physical_offset = physical_address.substr(physical_address.length() - 3, 3)
 
 		if physical_offset == expected_offset:
 			expected_pa = physical_address.substr(0, 5)
 			break
 
+	print("Comparação:")
+	print("Input PA: ", input_pa, " | Expected PA: ", expected_pa)
+	print("Input Offset: ", input_offset, " | Expected Offset: ", expected_offset)
+
 	if input_pa == expected_pa and input_offset == expected_offset:
 		label_check.text = "Tradução correta!"
+		winsfx.play()
 		current_index += 1
 		update_virtual_address()
 		le_pa.text = ""
 		le_offset.text = ""
 	else:
 		label_check.text = "Tradução incorreta!\nTente novamente."
+		losesfx.play()
