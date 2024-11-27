be94c783de92dfd593e7955ea5d9871ea2e9e893
hugobiella <hugo.a.biella@hotmail.com>
2024-11-27 01:02:52 -0300

    updates 27/11

--- new-game-project/scenes/ui/input_manager.gd
+++ new-game-project/scenes/ui/input_manager.gd
@@ -3,26 +3,22 @@ extends Node2D
 func _on_button_pressed():
 	var line_edit = $LineEdit
 	var input_text = line_edit.text.strip_edges()
-	
-	# Verifica se a entrada é vazia ou contém caracteres não numéricos
+
 	if input_text == "" or not _is_valid_integer(input_text):
 		line_edit.text = ""
 		line_edit.placeholder_text = "Entrada inválida"
 		return
-	
-	# Converte a entrada para inteiro e verifica o limite
+
 	var input_value = int(input_text)
-	if input_value > 12: # Máximo permitido
+	if input_value > 12: #max memories in game
 		line_edit.text = ""
 		line_edit.placeholder_text = "Valor Inválido"
 	else:
 		Globals.amount_to_generate = input_value
 		get_tree().current_scene._on_input_received(Globals.amount_to_generate)
 
-# Função auxiliar para verificar se a string é um número inteiro válido
 func _is_valid_integer(value: String) -> bool:
 	for char in value:
-		# Verifica se cada caractere está entre '0' e '9'
 		if char < '0' or char > '9':
 			return false
 	return true
